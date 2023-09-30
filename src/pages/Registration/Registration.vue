9039f72bff44e3c3a89953e4a4fffb1b2352d53a
Maks <kiva_hill@mail.ru>
2023-09-30 16:46:26 +0300

    feat(font): add font

--- src/pages/Registration/Registration.vue
+++ src/pages/Registration/Registration.vue
@@ -4,6 +4,23 @@
 
 <template>
   <section class="registration">
+    <div class="registration__sign">
+      <div class="logo-block">
+        <img
+          class="logo-block__icon"
+          src="/src/assets/images/authed/sign-up-bg.webp"
+          alt="logo"
+        >
 
+        <span class="logo-block__text">Log in</span>
+      </div>
+    </div>
+
+    <div class="registration__promo">
+      <img
+        src=""
+        alt=""
+      >
+    </div>
   </section>
 </template>
